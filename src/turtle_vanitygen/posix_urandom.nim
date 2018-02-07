e71694672a21337c47e2858ee39526b0ba23f032
nnamon <nnamon@users.noreply.github.com>
2018-02-08 04:43:46 +0800

    Added IOError as a possible thrown exception to posix_urandom.nim to avoid error compiling on Mac OS (#1)

--- src/turtle_vanitygen/posix_urandom.nim
+++ src/turtle_vanitygen/posix_urandom.nim
@@ -2,15 +2,15 @@
 # License: MIT
 
 
-proc urandom*(size: Natural): seq[uint8] {.raises: [EOS, EOutOfMemory].} =
+proc urandom*(size: Natural): seq[uint8] {.raises: [EOS, EOutOfMemory, IOError].} =
     ## Reads and returns ``size`` bytes from the file ``/dev/urandom``.
     ## Raises ``EOS`` on failure.
     new_seq(result, size)
-    
+
     var file: TFile
     if not file.open("/dev/urandom"):
         raise new_exception(EOS, "/dev/urandom is not available")
-    
+
     var index = 0
     while index < size:
         let bytes_read = file.read_buffer(addr result[index], size-index)
